5d7f579f18aa40429ba9aa3264bba975c134f3de
tamir avraham <tamir.avrah@gmail.com>
2023-05-21 23:14:53 +0300

    added all of the new request types

--- Backend/Project1/responce.h
+++ Backend/Project1/responce.h
@@ -1,16 +1,37 @@
 #pragma once
 #include "requests.h"
+
+#define ResponceStruct(responce_name) struct responce_name :Responce{ responce_name()=default; }
 class IRequestHandler;
 struct Responce
 {
 	Buffer buffer;
 	IRequestHandler* next;
+
+	Responce(const Buffer& buffer, IRequestHandler* next)
+		: buffer(buffer), next(next)
+	{
+	}
 };
 struct LoginResponce :Responce
 {
-	
+	LoginResponce() = default;
 };
 struct SignUpResponce : Responce
 {
-	
+	SignUpResponce() = default;
 };
+struct LogoutResponce:Responce
+{
+	LogoutResponce() = default;
+};
+struct GetRoomsResponce:Responce
+{
+	GetRoomsResponce() = default;
+};
+struct GetHighScoreResponce :Responce {
+	GetHighScoreResponce() = default;
+};
+ResponceStruct(CreateRoomResponce);
+ResponceStruct(GetPersonalStatsResponce);
+ResponceStruct(JoinRoomResponce);
