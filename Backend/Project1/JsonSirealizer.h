01b7075518cc20ec552a7f8ff7c6a43738daeeb9
tamir avraham <tamir.avrah@gmail.com>
2023-05-21 23:41:59 +0300

    started working on a new sirealzier

---
+++ Backend/Project1/JsonSirealizer.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <concepts>
+#include "responce.h"
+template<class T>
+concept stringfy = requires(T v){
+	{v}->std::convertible_to<std::string>;
+};
+template<class T>
+concept canBeToStringed = requires(T v) {
+	{v.toString()}->std::convertible_to<std::string>;
+};
+class JsonSirealizer
+{
+public:
+private:
+	template<class T> 
+	static std::string getVectorAsString(std::vector<T> vector) requires stringfy<T>;
+	template<class T> 
+	static std::string getVectorAsString(std::vector<T> vector) requires canBeToStringed<T>;
+};
+
+template<class T>
+inline std::string JsonSirealizer::getVectorAsString(std::vector<T> vector) requires stringfy<T> 
+{
+	std::string ret = "[";
+	for (size_t i = 0; const auto & item : vector) {
+		ret += item;
+		ret += (++i == vector.size() ? ']' : ',');
+	}
+	return ret;
+}
+
+template<class T>
+inline std::string JsonSirealizer::getVectorAsString(std::vector<T> vector) requires canBeToStringed<T>
+{
+	std::string ret = "[";
+	for (size_t i = 0; const auto & item : vector) {
+		ret += item.toString();
+		ret += (++i == vector.size() ? ']' : ',');
+	}
+	return ret;
+}
